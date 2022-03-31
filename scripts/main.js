2f17deed1b8e2bee2bb6011fdc08b1618ffb7151
Héctor Buruca <torrezhectorb@gmail.com>
2022-03-30 22:57:46 -0600

    add work for JavaScript part 2

--- scripts/main.js
+++ scripts/main.js
@@ -147,3 +147,146 @@ function objectFunction(name, age) {
 }
 const funcObject = objectFunction('Second Custom Object', 20)
 console.log(funcObject)
+
+//Ejemplo de encapsulacion
+
+function objectFact() {
+    let _name = 'Test';
+    let _age = 10;
+    return {
+        height: '160cm',
+        name: function() {
+            return _name;
+        },
+        age: function() {
+            return _age;
+        },
+    }
+}
+
+const funcObjectEncap = objectFact();
+console.log(funcObjectEncap.age());
+
+//clousure
+
+function clousure1() {
+    const _height = '160cm';
+    const _rank = 'Sr.';
+    const privateFunct = function() {
+        console.log('Hola');
+    }
+    return function(id) {
+        if (id < 1) {
+            console.log('No hay nadie registrado');
+        } else {
+            return {
+                name: 'Sr. Nicolas',
+                age: 56,
+                info: function() {
+                    privateFunct();
+                    console.log('Mi rango es ' + _rank, 'Mi altura es ' + _height);
+                }
+            }
+        }
+        return {
+
+        }
+    }
+};
+
+const human = clousure1(null, 0);
+console.log(human(4).info());
+
+//Metodos
+const objecWitHMethods = {
+    name: 'Rick',
+    age: 10,
+    greet: function(name) {
+        return 'Hola' + name + ', mi nombre es ' + this.name;
+    },
+    aboutMe: function(info) {
+        return () => {
+            if (10 > info) {
+                return 'Hola señor, mi nombre es ' + this.name;
+            } else {
+                return 'Hola! mi nombre es ' + this.name;
+            }
+        }
+    }
+}
+
+console.log(objecWitHMethods.aboutMe()(1));
+
+const catPrototype = {
+    name: 'Tom',
+    sex: 'M',
+    age: 4,
+    miau() {
+        console.log(this.name)
+    }
+};
+
+const tom = Object.create(catPrototype);
+tom.name = 'Lucas';
+console.log(tom.miau());
+
+//Poliforfismo
+
+const shape = {
+    height: 0,
+    width: 0,
+    area() {
+
+    }
+}
+
+const triangle = Object.create(shape);
+triangle.height = 10;
+triangle.width = 4;
+triangle.area = function() {
+    return (this.height * this.width) / 2
+}
+
+const rectangle = Object.create(shape);
+rectangle.height = 10;
+rectangle.width = 4;
+rectangle.area = function() {
+    return this.heght * this.width;
+}
+rectangle.name = 'Rectangulo nuevo';
+console.log(triangle.area(), rectangle.area(), rectangle.name);
+
+
+class Tree {
+    _height;
+    _age;
+    _multiplier;
+    constructor(height, age) {
+        this._height = height;
+        this._age = age;
+        this._multiplier = this._multiplier * 0.5;
+    }
+    get age() {
+        return this._age;
+    }
+
+    set age(newAge) {
+        this._age = newAge;
+    }
+
+    get currentHeight() {
+        return this._heigth * this._age * this._multiplier;
+    }
+
+    static doc() {
+        return 'Esta es la clase prototipo para un arbol basico';
+    }
+}
+
+console.log(Tree.doc())
+
+const appleTree = new Tree(5, 1);
+console.log(appleTree.age, appleTree.currentHeight);
+
+appleTree.age = 10;
+console.log(appleTree.currentHeight)
